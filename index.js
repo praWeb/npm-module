8d51f881ed3d447e310eef034fc7e88673af8b00
praWeb <prasanna.chowdary@matalandirect.com>
2016-06-09 16:45:40 +0100

    made the module loading easier now

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const values= {
-  "one" : 1,
-  "two" : 2
-};
+(function() {
 
-module.exports = {
-  add: function(){
-    return values[one]+values[two];
-  }
-}
+  const values= {
+    "one" : 1,
+    "two" : 2
+  };
+  
+  // Add will be the class which we will be importing in our files
+  var Add = (function() {
+    function add() {
+        return values[one]+values[two];
+    };
+  });
+
+  // This is because 'module' loading is not yet supporting by a browser. 
+  // so we would be requiring a module loader to load module format ones
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = Add;
+  else
+    window.Add = Add;
+  
+})();
